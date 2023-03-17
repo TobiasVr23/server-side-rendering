5c1eaa69451f5e0e70374d1099a11324ab13347f
Tobias Vrolijk <vrolijktobias@gmail.com>
2023-03-17 10:15:56 +0100

    push

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
 // Importeer express uit de node_modules map
-import express from 'express'
-
-const url = 'https://whois.fdnd.nl/api/v1/member/tobias-vrolijk'
-const data = await fetch(url).then((response) => response.json())
+import { render } from "ejs";
+import express, { response } from "express";
 
 // Maak een nieuwe express app aan
-const app = express()
+const app = express();
+
+// get info form api
+
+const url = "https://api.visualthinking.fdnd.nl/api/v1";
 
 // Stel ejs in als template engine en geef de 'views' map door
-app.set('view engine', 'ejs')
-app.set('views', './views')
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 // Gebruik de map 'public' voor statische resources
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-// Maak een route voor de indexn
-app.get('/', function (req, res) {
-  // res.send('Hello World!')
-  res.render('index', data)
-})
+// Maak een route voor de inde
+
+app.get("/", (request, response) => {
+  console.log(request.query.methods);
+  const methodsUrl = url + "/methods";
+
+  fetchJson(methodsUrl).then((data) => {
+    response.render("index", data);
+  });
+});
 
 // Stel het poortnummer in waar express op gaat luisteren
-app.set('port', process.env.PORT || 8000)
+app.set("port", process.env.PORT || 8000);
 
 // Start express op, haal het ingestelde poortnummer op
-app.listen(app.get('port'), function () {
+app.listen(app.get("port"), function () {
   // Toon een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error);
+}
